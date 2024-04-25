7d906ae365818eb26e3ca718f1d92dabd46bf5df
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-04-25 17:11:31 +0200

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,40 @@
+const grade = 7;
+if (grade < 0) {
+
+console.log("¡Has aprobado!");
+}
+else {
+
+console.log("Has suspendido");
+
+}
+
+// Variables + Funciones // 
+
+let userName = "Juan";
+let userAge = 20;
+function saludar() {
+    console.log("Hola");
+}
+function despedir() {
+    return "Adiós";
+}
+
+if (userName === "Juan") {
+    console.log(saludar)
+}
+
+else {
+    console.log(despedir);
+}
+
+// 
+
+let option1 = "a"
+let option2 = "b"
+let resultado = sumar();
+
+function sumar() {
+    console.log(option1 + ', ' + option2);
+}
+
